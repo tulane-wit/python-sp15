a932cae525041bcbc7e1a9f1334090abef83b02a
jaelle <jaelle@gmail.com>
2015-02-24 21:49:21 +0000

    Added Week 1

---
+++ week1/hello_world.py
@@ -0,0 +1,10 @@
+# Print a string to the command line
+print "Hello World"
+
+# Ask a user for input from the command line
+# and save it to a variable
+x = raw_input("What is your name? ")
+
+# Print out the input you just collected
+# from the user
+print "Hello " + x
