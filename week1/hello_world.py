b90b6f76af8c6c63527b6f05dffe57c14dc3cdac
jaelle <jaelle@gmail.com>
2015-02-24 21:58:39 +0000

    Minor update to Week 1

--- week1/hello_world.py
+++ week1/hello_world.py
@@ -3,8 +3,8 @@ print "Hello World"
 
 # Ask a user for input from the command line
 # and save it to a variable
-x = raw_input("What is your name? ")
+name = raw_input("What is your name? ")
 
 # Print out the input you just collected
 # from the user
-print "Hello " + x
+print "Hello " + name
