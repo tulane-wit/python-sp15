9ef770c0c1b00859729fdec956306c12b5050f98
Jaelle Scheuerman <jaelle@gmail.com>
2015-02-28 14:00:23 -0600

    week 2 code added

---
+++ week02/variables.py
@@ -0,0 +1,22 @@
+# This program creates a variable named sample, and assigns it values of different data types.
+
+
+
+# To start, let's make sample a number! It can be positive, negative, zero, whole, or decimal.
+
+sample = 3
+
+# Now the variable sample has been assigned a value of 3.
+
+
+
+# Now let's assign sample a string value instead of a number value.
+# A string can be any letter, word, phrase, or sentence, as long as it's between quotation marks.
+# (Sidenote: this means that you can't put a quotation mark *inside* of a string.)
+
+sample = "cat"
+
+# Last, let's assign a Boolean (True or False) value to sample.
+# Make sure the first letter is capitalized or the computer won't realize it's a Boolean.
+
+sample = True
